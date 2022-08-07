17988bc11e93b12348fea41007215493c1c0d328
earl-stephens <stephensearl@hotmail.com>
2022-08-06 22:34:14 -0600

    Initial commit

---
+++ src/application/App.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.io.IOException;
+
+public class App {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
